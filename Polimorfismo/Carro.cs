43a54853bee48e6c1547f9ee95beb71687111cb3
Wesley Anhaia <wesleyanhaiaa@gmail.com>
2022-11-30 17:17:41 -0300

    adicionando novo projeto

---
+++ Polimorfismo/Carro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    internal class Carro: Veiculo
+    {
+       override public void Anda()
+        {
+            base.Anda(); /*/ quando é utilizado o comando "base" ele está se referindo a Classe mãe
+                          * ou seja, aqui ele vai chamar a função Anda() da classe mãe ue vai executar
+                          * a impressão "O veiculo está andando" antes do "O carro está andando" 
+                          * que está logo abaixo/*/
+            Console.WriteLine("O carro está andando");
+        }
+         
+    }
+}
---
+++ Polimorfismo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro objCarro = new Carro();
+            Bicicleta objBicicleta = new Bicicleta();
+
+            objCarro.Anda();     /*/Até esse momento do codigo não tinha nada nas classes carro e bicicleta
+                                  * então as duas classes só herdaram mo metodo Anda() da classe mãe Veiculo
+                                  * e executaram o código de impressão que está dentro do metodo./*/
+            objBicicleta.Anda();
+
+            Console.ReadKey(); /*/Explicação: Nesse exemplo de polimorfismo o carro e a bicileta estao
+                                * chamando a função Anda(), mas se tu for ver, as 3 classes tem a mesma
+                                * função dentro delas, porém, quando o objeto de carro por exemplo, chamar
+                                * a função Anda() no main, ele não vai imprimir a mensagem da Classe mãe(que é:
+                                * o veiculo está andando), e sim a mensagem dentro da classe filha(que
+                                * é: o carro esta andando) MAAASSSSS não é simplesmente copiar a função que
+                                * está na classe mãe e jogar na classe filha q isso vai funcionar, pra dar certo
+                                * tem que escrever o comando "virtual" na frente do modificador de acesso da
+                                * função da classe mãe e "override" na frente do modifcador de acesso da função das
+                                * classes filhas que nesse caso são a bicicleta e o carro /*/
+                                 
+
+
+        }
+    }
+}
