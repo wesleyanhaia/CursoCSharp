1f3b48953dfd25076cc7bee8a65e940be370ff64
Wesley Anhaia <wesleyanhaiaa@gmail.com>
2022-12-06 16:06:18 -0300

    Adicionando

---
+++ CarregandoDLL/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DLL;
+
+namespace Carregando_DLL_no_projeto
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+
+            /*/Para carregar a DLL no program eu cliquei com o botão direito em cima de referencias, que está
+             * localizado no grenciador de soluções, depois eu cliquei em adicionar referencia, depois fui em
+             * procurar, e tive que encontrar o debug da minha DLL que no caso foi facil pq ta tudo sendo
+             * salvo dentro da pasta do Curso# (Lembrando que eu tive que compilar a DLL sem executar ela,
+             * que dai ela cria esse arquivo que eu coloco aqui no program como referencia/*/
+
+        {
+            Console.WriteLine("A soma de 5 e 6 é: " + Matematica.Soma(5, 6)); 
+
+            /*/Essas duas funções que foram chamadas (a de somar e a de multiplicar) estão dentro da
+             * DLL é bom notar que elas foram chamadas através da classe que também está dentro da DLL
+             * é como se a DLL fosse uma extenção do arquivo principal/*/
+
+
+            Console.WriteLine("A multiplicação de 5 e 6 é: " + Matematica.Multiplica(5, 6));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DLL/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL
+    /*/O primeiro passo pra criar um DLL é na hora de criar um projeto, pq a gente nao
+     * cria um "Aplicativo do console" mas sim uma "Biblioteca de classes(.NET Framework)"
+      * e pra criar é só selecionar biblioteca na ultima caixinha de opções da direita/*/
+{
+    public class Matematica /*/ Aparentemente um DLL é igual uma classe, onde colocamos varios códigos dentro
+                             * e depois chamamos ela no program pra usar os codigos e comandos que estão la dentro.
+                             * Para chamar uma DLL no program primeiro precisamos adicionar o namespace dela
+                             * na parte das bibliotecas, no caso desta DLL em que estamos, seria using DLL;/*/
+    {
+        public static double Soma(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Multiplica(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}
